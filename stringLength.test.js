18f39771e907bbd7f0421465bce31581b7366ecb
rachid <rachidboudaoudi95@gmail.com>
2021-09-01 11:34:34 +0100

    Add a test for StringLength Error throw.

--- stringLength.test.js
+++ stringLength.test.js
@@ -1,5 +1,9 @@
 const stringLength = require('./stringLength');
 
-test('length of string', () => {
-  expect(stringLength('rachida')).toBe(7);
+test('length of a string', () => {
+  expect(stringLength('rachid')).toBe(6);
+});
+
+test('string length <=10 && >= 1', () => {
+  expect(() => stringLength('')).toThrow(Error);
 });
