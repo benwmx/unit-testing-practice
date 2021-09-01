11bd046453a3e75fe68eee3c5d9eb06983041a03
rachid <rachidboudaoudi95@gmail.com>
2021-09-01 13:27:50 +0100

    Add capitalize function

--- test/capitalize.test.js
+++ test/capitalize.test.js
@@ -4,8 +4,9 @@ describe('capitalize function', () => {
   test('should return a first-character capitalized string', () => {
     expect(capitalize('rachid')).toBe('Rachid');
     expect(capitalize('Rachid')).toBe('Rachid');
+    expect(capitalize('RACHID')).toBe('Rachid');
     expect(capitalize('&@//]@^')).toBe('&@//]@^');
-    expect(capitalize('r')).toBe(R);
+    expect(capitalize('r')).toBe('R');
     expect(capitalize('')).toBe('');
   })
 });
